6cfb370f272a72aa03524a8c522d710e52d97af0
Pedro <109972071+pedroplvs@users.noreply.github.com>
2022-09-23 10:50:48 -0300

    .

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ function load () {
     var date = new Date()
     var hour = date.getHours ()
     var minutes = date.getMinutes ()
-
     msg.innerHTML = `Agora são: ${hour} horas e ${minutes} minutos.`
     if (hour >= 0 && hour <12) {
-        image.src = 'manha-pr.png'
+        image.src = '/imagens/manha-pr.png'
         document.body.style.background = '#6AD9D9'
     } else if (hour>= 12 && hour <18) {
-        image.src = 'tarde-pr.png'
+        image.src = '/imagens/tarde-pr.png'
         document.body.style.background = "#F2A413"
     } else {
-        image.src = 'noite-pr.png'
+        image.src = '/imagens/noite-pr.png'
         document.body.style.background = "#013440"
     }
 }
